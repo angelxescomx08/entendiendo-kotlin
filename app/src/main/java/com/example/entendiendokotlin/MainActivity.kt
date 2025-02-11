633f3210b0da9fdd7131037bef8ad92952d3f2f9
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-02-10 19:09:12 -0600

    Create counter in kotlin

--- app/src/main/java/com/example/entendiendokotlin/MainActivity.kt
+++ app/src/main/java/com/example/entendiendokotlin/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.entendiendokotlin.ui.theme.EntendiendoKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             EntendiendoKotlinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Content()
                 }
             }
         }
@@ -31,17 +42,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Content(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Counter(10)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    EntendiendoKotlinTheme {
-        Greeting("Android")
+fun Counter(value: Int){
+    var counter by remember {
+        mutableStateOf(value)
+    }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ){
+        Button(onClick = {
+            counter--
+        }) {
+            Text(text = "-1")
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = counter.toString())
+        Spacer(modifier = Modifier.width(16.dp))
+        Button(onClick = {
+            counter++
+        }) {
+            Text(text = "+1")
+        }
     }
 }
